ee2a3955cfcf9993e0e49a35ce6f1cff96e00f38
GianfrancoTrancillo <gianfrancotrancillo@gmail.com>
2021-06-29 11:07:18 -0300

    vercel

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+const app = express();
+
+app.get("/ejemplo", (req, res) => {
+    res.end("Llegaste al ejemplo");
+});
+ 
+app.get("/usuarios", (req, res) => {
+    res.end("Llegaste a usuarios");
+});
+
+
+
+app.listen(3000, () => console.log(`Servidor corriendo en el puerto!`));
